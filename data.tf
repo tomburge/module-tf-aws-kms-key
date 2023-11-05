e4f19157d6b601c7636925120a13ae900f38fe62
Tom Burge <tom@tomburge.org>
2023-11-05 17:04:44 -0600

    updated key policy

--- data.tf
+++ data.tf
@@ -17,7 +17,7 @@ data "aws_iam_policy_document" "key_policy" {
   }
 
   dynamic "statement" {
-    for_each = { for target in var.targets : "default" => target if target.target_type == "cw_loggroup" }
+    for_each = { for target in var.targets : "cw_loggroup" => target if target.target_type == "cw_loggroup" }
     content {
       actions = [
         "kms:Decrypt",
